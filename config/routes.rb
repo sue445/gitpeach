614ccf139f7c779c4e32eed98946624406f93a2f
sue445 <sue445@sue445.net>
2014-01-10 23:17:56 +0900

    be rails g controller top index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Gitpeach::Application.routes.draw do
+  get "top/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
