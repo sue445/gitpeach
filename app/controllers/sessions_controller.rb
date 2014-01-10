5c4eef0c3a911cdbc373da69a84e0ff901bf3912
sue445 <sue445@sue445.net>
2014-01-11 02:06:07 +0900

    be rails g controller sessions create destroy

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Gitpeach::Application.routes.draw do
+  get "sessions/create"
+  get "sessions/destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
