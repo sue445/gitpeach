c520e42a784926b92b1e753c23ad3672c91db112
sue445 <sue445@sue445.net>
2014-02-01 22:44:16 +0900

    Show other person kanbans

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,8 +2,11 @@ class TopController < ApplicationController
   def index
     if current_user
       projects = current_user.projects
-      @kanban_projects    = projects.select{|project| Kanban.exists?(gitlab_project_id: project.id) }
-      @no_kanban_projects = projects.reject{|project| Kanban.exists?(gitlab_project_id: project.id) }
+      @user_kanban_projects = projects.select{|project| Kanban.exists?(gitlab_project_id: project.id) }
+      @no_kanban_projects   = projects.reject{|project| Kanban.exists?(gitlab_project_id: project.id) }
+
+      user_project_ids = @user_kanban_projects.map(&:id)
+      @other_kanbans = Kanban.where.not(gitlab_project_id: user_project_ids)
     end
   end
 end
