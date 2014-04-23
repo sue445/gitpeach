f481279c5ae000a3b3c471c88df5b7cbb79d7b0c
sue445 <sue445@sue445.net>
2014-04-24 02:34:29 +0900

    Setting precompile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.assets.precompile += %w( *.js )
 end
 
 # indent slim view
